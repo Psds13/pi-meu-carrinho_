23d5c2853eb870d24dbcaca9d935e291e827daa5
MyckeDev <lemosmyckael@gmail.com>
2024-11-21 16:52:01 -0300

    Ajuste no js da topnav

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,11 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  function toggleNav() {
+  var nav = document.getElementById("myTopnav");
+  if (nav.style.display === "block") {
+    nav.style.display = "none"; // Esconde o menu
+  } else {
+    nav.style.display = "block"; // Exibe o menu
+  }
+}
