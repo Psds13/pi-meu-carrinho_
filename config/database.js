4d6a3cc23366c63423c0613c62c7550954ed2810
PedroPH0 <pedrohenriquecostamesquita@gmail.com>
2024-12-01 22:31:45 -0300

    adcionando direcionamento nos botoes de categorias nas paginas(mateus, assai, atacadao)

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'senac123456789',
+    password: '',
     database: 'meucarrinho'
 });
 
